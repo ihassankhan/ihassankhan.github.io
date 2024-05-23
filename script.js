b642ed78537eb4c6d7151239ad1370a68a6337a7
ihassankhan <134612017+ihassankhan@users.noreply.github.com>
2024-05-23 06:30:47 +0000

    Added first section home page with responsive

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ window.addEventListener("scroll", function() {
     let header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const textCol = document.querySelector('.fs__text__col');
+    const imgCol = document.querySelector('.fs__img__col');
+
+    window.addEventListener('scroll', () => {
+        let scrollY = window.scrollY;
+
+        // Check if the section is in the viewport
+        if (scrollY + window.innerHeight >= textCol.offsetTop) {
+            textCol.style.animation = 'fadeInUp 1s forwards ease-in-out';
+            imgCol.style.animation = 'fadeInUp 1s forwards ease-in-out 0.5s';
+        }
+    });
+});
